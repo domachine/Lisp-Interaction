9e566204fe68664fd046e854824336d009b45c4d
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-04-16 17:05:49 +0200

    Added list_next() function.

--- src/utils.hpp
+++ src/utils.hpp
@@ -2,28 +2,17 @@
 #define LISP_UTILS_HPP
 
 #include <boost/function.hpp>
+
+#include "lisp.hpp"
+
+
 namespace lisp {
     typedef boost::function<void (object_ptr_t, int)> callback_t;
 
-    void dolist(const cons_cell_ptr_t list, callback_t cb)
-    {
-        // const correctness
-        cons_cell_ptr_t _list = list;
+    void dolist(const cons_cell_ptr_t list, callback_t cb);
 
-        int i;
-        for(i = 0; _list && *_list; ++i) {
-            object_ptr_t current = _list->car();
-
-            cb(current, i);
-
-            object_ptr_t next = _list->cdr();
-            if(!next->is_cons_cell())
-                // If next is not a list throw its index.
-                throw i;
-
-            _list = boost::dynamic_pointer_cast<cons_cell>(next);
-        }
-    }
+    cons_cell_ptr_t list_next(cons_cell_ptr_t list,
+                              const std::string& msg = std::string());
 }
 
 #endif  // LISP_UTILS_HPP
