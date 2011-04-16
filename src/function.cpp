c492521754f8a38be54aeef0a41923ca78d2f000
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-04-16 17:03:49 +0200

    Beautified code and `wrong-number-of-arguments' issue fixture.

--- src/function.cpp
+++ src/function.cpp
@@ -1,7 +1,9 @@
 
 #include "function.hpp"
 
-#include "logging.hpp"
+#include <boost/foreach.hpp>
+
+#include "utils.hpp"
 
 
 namespace lisp {
@@ -15,66 +17,95 @@ namespace lisp {
 
     object_ptr_t function::operator()(environment* env, const cons_cell_ptr_t args)
     {
-        using logging::log;
-        using logging::DEBUG;
-
         // Create isolated environment.
         environment func_env(env);
 
-        arg_sym_list_t::const_iterator arg_sym_iter = m_arg_symbols.begin();
-
-        object_ptr_t cdr_ = nil();
-
-        if(args)
-            cdr_ = args;
+        cons_cell_ptr_t _args = list_next(args, args->car()->str() + ": listp");
 
         // Assign all given args to corresponding symbols
         // in the function environment.
-        while(cdr_ != nil()) {
-            object_ptr_t car_;
-
-            if(cdr_->is_cons_cell())
-                // Seems to be risky but it's not :-)
-                car_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->car();
-            else
-                signal(env->get_symbol("wrong-type-argument"),
-                       "listp " + cdr_->str());
-
-            if(arg_sym_iter == m_arg_symbols.end())
+        BOOST_FOREACH(const std::string& current, m_arg_symbols) {
+            if(!_args)
                 signal(env->get_symbol("wrong-number-of-arguments"),
                        args->car()->str());
 
             // Create new symbol in function environment.
-            symbol_ptr_t new_sym = func_env.create_symbol(*arg_sym_iter);
-            new_sym->set_value(env->eval(car_));
+            symbol_ptr_t new_sym = func_env.create_symbol(current);
+            new_sym->set_value(env->eval(_args->car()));
 
-            ++arg_sym_iter;
-
-            cdr_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->cdr();
+            _args = list_next(_args, args->car()->str() + ": listp");
         }
 
         object_ptr_t last_result = nil();
-        cdr_ = nil();
+        cons_cell_ptr_t _body = m_body;
 
-        if(m_body)
-            cdr_ = m_body;
-
-        while(cdr_ != nil()) {
-            object_ptr_t car_;
-
-            if(cdr_->is_cons_cell())
-                // Seems to be risky but it's not :-)
-                car_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->car();
-            else
-                signal(env->get_symbol("wrong-type-argument"),
-                       "listp " + cdr_->str());
-
-            last_result = func_env.eval(car_);
-            cdr_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->cdr();
+        while(_body) {
+            last_result = func_env.eval(_body->car());
+            _body = list_next(_body, args->car()->str() + ": listp");
         }
 
         return last_result;
     }
+
+    // object_ptr_t function::operator_bak(environment* env, const cons_cell_ptr_t args)
+    // {
+    //     // Create isolated environment.
+    //     environment func_env(env);
+
+    //     arg_sym_list_t::const_iterator arg_sym_iter = m_arg_symbols.begin();
+
+    //     object_ptr_t cdr_ = nil();
+
+    //     if(args)
+    //         cdr_ = args->cdr();
+
+    //     // Assign all given args to corresponding symbols
+    //     // in the function environment.
+    //     while(cdr_ != nil()) {
+    //         object_ptr_t car_;
+
+    //         if(cdr_->is_cons_cell())
+    //             // Seems to be risky but it's not :-)
+    //             car_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->car();
+    //         else
+    //             signal(env->get_symbol("wrong-type-argument"),
+    //                    "listp " + cdr_->str());
+
+    //         if(arg_sym_iter == m_arg_symbols.end())
+    //             signal(env->get_symbol("wrong-number-of-arguments"),
+    //                    args->car()->str());
+
+    //         // Create new symbol in function environment.
+    //         symbol_ptr_t new_sym = func_env.create_symbol(*arg_sym_iter);
+    //         new_sym->set_value(env->eval(car_));
+
+    //         ++arg_sym_iter;
+
+    //         cdr_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->cdr();
+    //     }
+
+    //     object_ptr_t last_result = nil();
+    //     cdr_ = nil();
+
+    //     if(m_body)
+    //         cdr_ = m_body;
+
+    //     while(cdr_ != nil()) {
+    //         object_ptr_t car_;
+
+    //         if(cdr_->is_cons_cell())
+    //             // Seems to be risky but it's not :-)
+    //             car_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->car();
+    //         else
+    //             signal(env->get_symbol("wrong-type-argument"),
+    //                    "listp " + cdr_->str());
+
+    //         last_result = func_env.eval(car_);
+    //         cdr_ = boost::dynamic_pointer_cast<cons_cell>(cdr_)->cdr();
+    //     }
+
+    //     return last_result;
+    // }
 
     std::string function::str() const
     {
@@ -85,13 +116,13 @@ namespace lisp {
         return os.str();
     }
 
-    object_ptr_t defun_form::operator()(environment* env, const cons_cell_ptr_t args)
+    object_ptr_t lambda_form::operator()(environment* env, const cons_cell_ptr_t args)
     {
         cons_cell_ptr_t cdr = boost::dynamic_pointer_cast<cons_cell>(args->cdr());
         object_ptr_t arg_list = cdr->car();
 
         if(!arg_list->is_cons_cell() && arg_list != nil())
-            signal(env->get_symbol("wrong-type-argument"), "listp <argument-1 to defun>");
+            signal(env->get_symbol("wrong-type-argument"), "listp <argument-1 to lambda>");
 
         function::arg_sym_list_t function_arg_list;
         cons_cell_ptr_t body = boost::dynamic_pointer_cast<cons_cell>(cdr->cdr());
