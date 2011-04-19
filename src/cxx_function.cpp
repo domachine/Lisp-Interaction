26209da54ace4a6a3641cda314e6db75897b71af
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-04-19 08:59:12 +0200

    Implementation of cxx_function.hpp.

---
+++ src/cxx_function.cpp
@@ -0,0 +1,27 @@
+
+#include "cxx_function.hpp"
+
+#include "lisp.hpp"
+#include "utils.hpp"
+
+namespace lisp {
+    object_ptr_t cxx_function::operator()(environment* env,
+                                          const cons_cell_ptr_t args)
+    {
+        assert(args);
+        object_ptr_t func = args->car();
+        std::string errmsg = func->str() + ": listp";
+
+        std::vector<object_ptr_t> vargs;
+
+        cons_cell_ptr_t _args = list_next(args, errmsg);
+
+        while(_args) {
+            vargs.push_back(env->eval(_args->car()));
+
+            _args = list_next(_args, errmsg);
+        }
+
+        return (*this)(env, vargs);
+    }
+}
