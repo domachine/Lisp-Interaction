6dcee8147e9310b0668cb802c6b8fda6a6ff49e5
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-04-19 08:58:45 +0200

    Helper class to make form-writing easier.

---
+++ src/cxx_function.hpp
@@ -0,0 +1,18 @@
+#ifndef LISP_CXX_FUNCTION_HPP
+#define LISP_CXX_FUNCTION_HPP
+
+#include "object.hpp"
+
+namespace lisp {
+    class cxx_function : public object
+    {
+    protected:
+        object_ptr_t operator()(environment* env,
+                                const cons_cell_ptr_t args = cons_cell_ptr_t());
+
+        virtual object_ptr_t operator()(environment* env,
+                                        const std::vector<object_ptr_t>& args) = 0;
+    };
+}
+
+#endif  // LISP_CXX_FUNCTION_HPP
