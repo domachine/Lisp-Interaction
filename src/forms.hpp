366e66ec9738f24d61f30516913b96468a545e4e
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-04-19 09:00:07 +0200

    Implemented equal-form.

--- src/forms.hpp
+++ src/forms.hpp
@@ -106,6 +106,9 @@ namespace lisp {
             {
                 cons_cell_ptr_t _args = list_next(args, "fset: listp");
 
+                if(!_args)
+                    signal(env->get_symbol("wrong-number-of-arguments"), "setf");
+
                 object_ptr_t sym_ref = env->eval(_args->car());
 
                 if(!sym_ref->is_symbol_ref())
@@ -116,9 +119,163 @@ namespace lisp {
 
                 symbol_ptr_t sym = env->get_symbol(r_sym_ref->name());
 
-                sym->set_function(env->eval(list_next(_args, "fset: listp")->car()));
+                cons_cell_ptr_t value = list_next(_args, "fset: listp");
+
+                if(!value)
+                    signal(env->get_symbol("wrong-number-of-arguments"), "fset");
+
+                sym->set_function(env->eval(value->car()));
 
                 return sym;
+            }
+    };
+
+    class setf_form : public object
+    {
+    protected:
+        object_ptr_t operator()(environment* env,
+                                const cons_cell_ptr_t args = cons_cell_ptr_t())
+            {
+                cons_cell_ptr_t _args = list_next(args, "setf: listp");
+
+                if(!_args)
+                    signal(env->get_symbol("wrong-number-of-arguments"), "setf");
+
+                object_ptr_t sym_ref = env->eval(_args->car());
+
+                if(!sym_ref->is_symbol_ref())
+                    signal(env->get_symbol("wrong-type-argument"),
+                           "setf: symbolp");
+
+                symbol_ref_ptr_t r_sym_ref = boost::dynamic_pointer_cast<symbol_ref>(sym_ref);
+
+                symbol_ptr_t sym = env->get_symbol(r_sym_ref->name());
+
+                cons_cell_ptr_t value = list_next(_args, "setf: listp");
+
+                if(!value)
+                    signal(env->get_symbol("wrong-number-of-arguments"), "setf");
+
+                sym->set_value(env->eval(value->car()));
+
+                return sym;
+            }
+    };
+
+    class setq_form : public object
+    {
+    protected:
+        object_ptr_t operator()(environment* env,
+                                const cons_cell_ptr_t args = cons_cell_ptr_t())
+            {
+                cons_cell_ptr_t _args = list_next(args, "setq: listp");
+
+                if(!_args)
+                    signal(env->get_symbol("wrong-number-of-arguments"), "setq");
+
+                object_ptr_t sym_ref = _args->car();
+
+                if(!sym_ref->is_symbol_ref())
+                    signal(env->get_symbol("wrong-type-argument"),
+                           "setq: symbolp");
+
+                symbol_ref_ptr_t r_sym_ref = boost::dynamic_pointer_cast<symbol_ref>(sym_ref);
+
+                symbol_ptr_t sym = env->get_symbol(r_sym_ref->name());
+
+                cons_cell_ptr_t value = list_next(_args, "setq: listp");
+
+                if(!value)
+                    signal(env->get_symbol("wrong-number-of-arguments"), "setq");
+
+                sym->set_value(env->eval(value->car()));
+
+                return sym;
+            }
+    };
+
+    class defun_form : public object
+    {
+    protected:
+        object_ptr_t operator()(environment* env, const cons_cell_ptr_t args)
+            {
+                // Pop function object from top.
+                cons_cell_ptr_t cdr = list_next(args, "defun: listp");
+
+                if(!cdr)
+                    signal(env->get_symbol("wrong-number-of-arguments"),
+                           "defun");
+
+                // Determine function symbol.
+                object_ptr_t sym_raw = cdr->car();
+                if(!sym_raw->is_symbol_ref())
+                    signal(env->get_symbol("wrong-type-argument"),
+                           "symbolp <argument-1 to defun>");
+
+                symbol_ref_ptr_t sym_ref = boost::dynamic_pointer_cast<symbol_ref>(sym_raw);
+                symbol_ptr_t sym = env->get_symbol(sym_ref->name());
+
+                cdr = list_next(cdr, "defun: listp");
+
+                if(!cdr)
+                    signal(env->get_symbol("wrong-number-of-arguments"),
+                           "defun");
+
+                object_ptr_t arg_list = cdr->car();
+
+                if(!arg_list->is_cons_cell() && arg_list != nil())
+                    signal(env->get_symbol("wrong-type-argument"),
+                           "listp <argument-2 to defun>");
+
+                // Build functions from remaining arguments.
+                function::arg_sym_list_t function_arg_list;
+                cons_cell_ptr_t body = boost::dynamic_pointer_cast<cons_cell>(cdr->cdr());
+
+                if(arg_list->is_cons_cell()) {
+                    cons_cell_ptr_t arg_list_cell =
+                        boost::dynamic_pointer_cast<cons_cell>(arg_list);
+
+                    while(arg_list_cell && *arg_list_cell) {
+                        if(arg_list_cell->car()->is_symbol_ref()) {
+                            symbol_ref_ptr_t arg_sym =
+                                boost::dynamic_pointer_cast<symbol_ref>(arg_list_cell->car());
+
+                            function_arg_list.push_back(arg_sym->name());
+
+                            arg_list_cell =
+                                boost::dynamic_pointer_cast<cons_cell>(arg_list_cell->cdr());
+                        }
+                    }
+                }
+
+                // Manipulate symbol and return it.
+                sym->set_function(object_ptr_t(new function(function_arg_list, body)));
+                return sym;
+            }
+    };
+
+    class equal_form : public object
+    {
+    protected:
+        object_ptr_t operator()(environment* env,
+                                const cons_cell_ptr_t args = cons_cell_ptr_t())
+            {
+                cons_cell_ptr_t first = list_next(args, "equal: listp");
+
+                if(!first)
+                    signal(env->get_symbol("wrong-number-of-arguments"),
+                           "equal");
+
+                cons_cell_ptr_t second = list_next(first, "equal: listp");
+
+                if(!second)
+                    signal(env->get_symbol("wrong-number-of-arguments"),
+                           "equal");
+
+                if(env->eval(first->car()) == env->eval(second->car()))
+                    return t();
+                else
+                    return nil();
             }
     };
 }
