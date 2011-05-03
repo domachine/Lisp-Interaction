0029f895550403039fccac0d8f5193db6306c716
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-05-03 21:28:28 +0200

    Implemented comparison operator and rewrote the number classes.

--- src/numbers.hpp
+++ src/numbers.hpp
@@ -1,32 +1,57 @@
 #ifndef LISP_NUMBERS_HPP
 #define LISP_NUMBERS_HPP
 
+#include <sstream>
+
+#include "object.hpp"
+
 namespace lisp {
-    template <typename T>
+    /**
+       @brief Number interface.
+
+       Provides elementary numeric operations like
+       addition, multiplication, ...
+    */
     class number : public object
     {
     public:
-        number(T value)
-            : m_value(value)
+        virtual bool is_int() const
             {
+                return false;
             }
 
-        number(const std::string& str)
+        virtual bool is_float() const
             {
-                std::stringstream os;
-
-                os << str;
-                os >> m_value;
+                return false;
             }
 
         bool is_number() const
             {
                 return true;
             }
+    };
 
-        operator T() const
+    /**
+       @brief The integer implementation.
+       @todo Implement this using libgmp to provide arbitrary
+       large integers.
+    */
+    class int_number : public number
+    {
+    public:
+        typedef long value_type;
+
+        int_number(value_type v)
+            : m_value(v)
             {
-                return m_value;
+            }
+
+        int_number(const std::string& str)
+            {
+                std::stringstream os;
+
+                os << str;
+                os >> m_value;
             }
 
         std::string str() const
@@ -38,8 +63,47 @@ namespace lisp {
                 return os.str();
             }
 
+        bool is_int() const;
+        value_type value() const;
+        bool operator==(const object& other);
+
     private:
-        T m_value;
+        long m_value;
+    };
+
+    class float_number : public number
+    {
+    public:
+        typedef float value_type;
+
+        float_number(value_type v)
+            : m_value(v)
+            {
+            }
+
+        float_number(const std::string& str)
+            {
+                std::stringstream os;
+
+                os << str;
+                os >> m_value;
+            }
+
+        std::string str() const
+            {
+                std::stringstream os;
+
+                os << m_value;
+
+                return os.str();
+            }
+
+        bool is_float() const;
+        value_type value() const;
+        bool operator==(const object& other);
+
+    private:
+        value_type m_value;
     };
 }
 
