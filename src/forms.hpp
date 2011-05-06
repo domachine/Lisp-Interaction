23fed8ff6b58b0925270b69d398d8ca274b9cee6
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-05-06 13:34:00 +0200

    Adapt new number class.

--- src/forms.hpp
+++ src/forms.hpp
@@ -285,36 +285,23 @@ namespace lisp {
         object_ptr_t operator()(environment* env,
                                 const argv_t& args)
             {
-                float sum = 0.0;
+                number_ptr_t sum = number_ptr_t(new number(static_cast<long long>(0)));
                 size_t sz = args.size();
-                bool sum_is_int = true;
 
                 for(unsigned int i = 0; i < sz; i++)
                 {
                     if(args[i]->is_number())
                     {
+                        
                         number_ptr_t num = boost::dynamic_pointer_cast<lisp::number>(args[i]);
-                        if(num->is_int())
-                        {
-                            sum += boost::dynamic_pointer_cast<lisp::int_number>(num)->value();
-                        }
-                        else if(num->is_float())
-                        {
-                            sum += boost::dynamic_pointer_cast<lisp::float_number>(num)->value();
-                            sum_is_int = false;
-                        }
-                    }
 
-                    else
-                    {
-                        signal(env->get_symbol("wrong-type-argument"), "+: numberp " + args[i]->str());
+                        *sum = *sum + *num;
                     }
+                    else
+                        signal(env->get_symbol("wrong-type-argument"), "+: numberp " + args[i]->str());
                 }
 
-                if(sum_is_int)
-                    return int_number_ptr_t(new int_number(sum));
-
-                return float_number_ptr_t(new float_number(sum));
+                return sum;
             }
     };
 }
