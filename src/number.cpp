c4f9e05b5b8e9262846503ee22b1893d32eeced6
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-05-06 13:35:12 +0200

    Implementation of number.hpp.

---
+++ src/number.cpp
@@ -0,0 +1,331 @@
+// $Id: AnyScalar.cc 59 2007-07-17 14:43:23Z tb $
+
+/*
+ * STX Expression Parser C++ Framework v0.7
+ * Copyright (C) 2007 Timo Bingmann
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/** \file AnyScalar.cc
+ * Implementation of the typed scalar value class AnyScalar used by the parser
+ * to represent values.
+ */
+
+#include "number.hpp"
+
+#include <stdlib.h>
+#include <functional>
+#include <algorithm>
+
+#include <boost/lexical_cast.hpp>
+
+#include "arith_error.hpp"
+
+
+namespace lisp {
+    std::string number::get_type_string(attrtype_t at)
+    {
+        switch(at)
+        {
+        case ATTRTYPE_LONG:		return "long";
+        case ATTRTYPE_DOUBLE:	return "double";
+        case ATTRTYPE_FRACTION:	return "fraction";
+        }
+        return "unknown";
+    }
+
+    number& number::operator=(const long long &l)
+    {
+        atype = ATTRTYPE_LONG;
+        val._long = l;
+        return *this;
+    }
+
+    number& number::operator=(const double &d)
+    {
+        atype = ATTRTYPE_DOUBLE;
+        val._double = d;
+        return *this;
+    }
+
+    number& number::set_fraction(int z, int n)
+    {
+        atype = ATTRTYPE_FRACTION;
+        val._fraction.z = z;
+        val._fraction.n = n;
+        return *this;
+    }
+
+    long long number::as_long() const
+    {
+        switch(atype)
+        {
+        case ATTRTYPE_LONG:
+            return val._long;
+
+        case ATTRTYPE_DOUBLE:
+            return static_cast<long long>(val._double);
+
+        case ATTRTYPE_FRACTION:
+            return val._fraction.z/val._fraction.n;
+        }
+
+        assert(0);
+        return false;
+    }
+
+    double number::as_double() const
+    {
+        switch(atype)
+        {
+        case ATTRTYPE_LONG:
+            return static_cast<double>(val._long);
+
+        case ATTRTYPE_DOUBLE:
+            return val._double;
+
+        case ATTRTYPE_FRACTION:
+            return  static_cast<double>(val._fraction.z)/
+                static_cast<double>(val._fraction.n);
+        }
+        assert(0);
+        return false;
+    }
+
+    std::string number::str() const
+    {
+        switch(atype)
+        {
+        case ATTRTYPE_LONG:
+            return boost::lexical_cast<std::string>(val._long);
+    
+        case ATTRTYPE_DOUBLE:
+            return boost::lexical_cast<std::string>(val._double);
+
+        case ATTRTYPE_FRACTION:
+            return  boost::lexical_cast<std::string>(val._fraction.z)
+                + "/" +
+                boost::lexical_cast<std::string>(val._fraction.n);
+        }
+        assert(0);
+        return "";
+    }
+
+    bool number::convert_type(attrtype_t t)
+    {
+        // same source and target type?
+        if (atype == t) return true;
+
+        switch(t)
+        {
+    
+        case ATTRTYPE_LONG:
+        {
+            long long v = as_long();
+            val._long = v;
+            atype = t;
+            return true;
+        }
+
+        case ATTRTYPE_DOUBLE:
+        {
+            double d = as_double();
+            val._double = d;
+            atype = t;
+            return true;
+        }
+
+        //This doesn't convert double back to fraction,
+        //as it shouldn't be necessary
+        case ATTRTYPE_FRACTION:
+        {
+            int z = as_long();
+            val._fraction.z = z;
+            val._fraction.n = 1;
+        }
+ 
+        }
+        assert(0);
+        return false;
+    }
+
+    number number::operator-() const
+    {
+        number at = *this;
+    
+        switch(at.atype)
+        {
+        case ATTRTYPE_LONG:
+            at.val._long = - at.val._long;
+            break;
+
+        case ATTRTYPE_DOUBLE:
+            at.val._double = - at.val._double;
+            break;
+
+        case ATTRTYPE_FRACTION:
+            at.val._fraction.z = -at.val._fraction.z;
+        }
+
+        return at;
+    }
+
+    template <template <typename Type> class Operator, char OpName>
+    number number::binary_arith_op(const number &b) const
+    {
+        switch(atype)
+        {
+        case ATTRTYPE_LONG:
+        {
+            switch(b.atype)
+            {
+            case ATTRTYPE_LONG:
+            {
+                Operator<long long> op;
+                if (OpName == '/' && b.val._long == 0)
+                    throw arith_error("division by zero");
+
+                return number( op(val._long, b.val._long) );
+            }
+
+            case ATTRTYPE_DOUBLE:
+            {
+                Operator<double> op;
+                return number( op(static_cast<double>(val._long), b.val._double) );
+            }
+
+/*      Needs overloaded Operators for class fraction
+
+        case ATTRTYPE_FRACTION:
+        {
+        Operator<int> op;
+        if (OpName == '/' && b.val._long == 0) throw(ArithmeticException("Integer division by zero"));
+        number b2(*this);
+        b2.convertType(ATTRTYPE_FRACTION);
+        return number(op(val._long, b.val));
+        }
+*/
+            }
+            break;
+        }
+
+        case ATTRTYPE_DOUBLE:
+        {
+            switch(b.atype)
+            {
+	    
+            case ATTRTYPE_LONG:
+            {
+                Operator<double> op;
+                return number( op(val._double, static_cast<double>(b.val._long)) );
+            }
+
+            case ATTRTYPE_DOUBLE:
+            {
+                Operator<double> op;
+                return number( op(val._double, b.val._double) );
+            }
+            }
+
+            break;
+        }
+
+        }
+        assert(0);
+        //throw(ConversionException("Invalid binary operator call. (PROGRAM ERROR)"));
+    }
+
+// *** Force instantiation of binary_op for the four arithmetic operators
+
+/// Forced instantiation of binary_arith_op for number::operator+()
+    template number number::binary_arith_op<std::plus, '+'>(const number &b) const;
+
+/// Forced instantiation of binary_arith_op for number::operator-()
+    template number number::binary_arith_op<std::minus, '-'>(const number &b) const;
+
+/// Forced instantiation of binary_arith_op for number::operator*()
+    template number number::binary_arith_op<std::multiplies, '*'>(const number &b) const;
+
+/// Forced instantiation of binary_arith_op for number::operator/()
+    template number number::binary_arith_op<std::divides, '/'>(const number &b) const;
+
+    template <template <typename Type> class Operator, int OpNum>
+    bool number::binary_comp_op(const number &b) const
+    {
+        // static const char *OpNameArray[] = { "==", "!=", "<", ">", "<=", ">=" };
+
+        switch(atype)
+        {
+        case ATTRTYPE_LONG:
+        {
+            switch(b.atype)
+            {
+            case ATTRTYPE_LONG:
+            {
+                Operator<long long> op;
+                return op(val._long, b.val._long);
+            }
+            case ATTRTYPE_DOUBLE:
+            {
+                Operator<double> op;
+                return op(static_cast<double>(val._long), b.val._double);
+            }
+            }
+            break;
+        }
+
+        case ATTRTYPE_DOUBLE:
+        {
+            switch(b.atype)
+            {
+            case ATTRTYPE_LONG:
+            {
+                Operator<double> op;
+                return op(val._double, static_cast<double>(b.val._long));
+            }
+
+            case ATTRTYPE_DOUBLE:
+            {
+                Operator<double> op;
+                return op(val._double, b.val._double);
+            }
+            }
+            break;
+        }
+
+        }
+        assert(0);
+        //throw(ConversionException("Invalid binary operator call. (PROGRAM ERROR)"));
+    }
+
+/// Forced instantiation of binary_comp_op for number::equal_to()
+    template bool number::binary_comp_op<std::equal_to, 0>(const number &b) const;
+
+/// Forced instantiation of binary_comp_op for number::not_equal_to()
+    template bool number::binary_comp_op<std::not_equal_to, 1>(const number &b) const;
+
+/// Forced instantiation of binary_comp_op for number::less()
+    template bool number::binary_comp_op<std::less, 2>(const number &b) const;
+
+/// Forced instantiation of binary_comp_op for number::greater()
+    template bool number::binary_comp_op<std::greater, 3>(const number &b) const;
+
+/// Forced instantiation of binary_comp_op for number::less_equal()
+    template bool number::binary_comp_op<std::less_equal, 4>(const number &b) const;
+
+/// Forced instantiation of binary_comp_op for number::greater_equal()
+    template bool number::binary_comp_op<std::greater_equal, 5>(const number &b) const;
+}
